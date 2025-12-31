f44a6839a62b87e41fcaac712b1627fda2f3d90f
Ishika Patel <ishikapatel1364@gmail.com>
2025-12-31 19:26:00 +0530

    fix: router outlet in app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './core/layout/auth-layout/auth-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/client/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./core/layout/auth-layout/auth-layout').then((m) => m.AuthLayout),
+  },
+];
