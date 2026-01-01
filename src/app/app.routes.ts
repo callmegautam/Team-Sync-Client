e1642ca3b16acf1426bb979583f4f524338c2397
Gautam Suthar <iamgautamsuthar@outlook.com>
2026-01-01 21:50:50 +0530

    add: basic router setup with placeholder and example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,50 @@
 import { Routes } from '@angular/router';
-import { AuthLayout } from './core/layout/auth-layout/auth-layout';
+
+// LAZY LOAD COMPONENT EXAMPLE
+// {
+//   path: 'dashboard',
+//   loadComponent: () =>
+//     import('./layouts/dashboard-layout/dashboard-layout.component').then(
+//       (m) => m.DashboardLayoutComponent
+//     ),
+// }
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/client/home/home').then((m) => m.Home),
+    // add Home page component
   },
   {
-    path: 'login',
-    loadComponent: () => import('./core/layout/auth-layout/auth-layout').then((m) => m.AuthLayout),
+    path: '',
+    // add Auth layout component
+    children: [
+      {
+        path: 'login',
+        // add Login page component
+      },
+      {
+        path: 'register',
+        // add Register page component
+      },
+    ],
+  },
+  {
+    path: 'dashboard',
+    // add Dashboard layout component
+    children: [
+      {
+        path: '',
+        // add Dashboard page component
+      },
+      {
+        path: 'tasks',
+        // add Tasks page component
+      },
+      // and so on...
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
