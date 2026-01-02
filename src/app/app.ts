18e45174cf1c5e5f84c538aac9df536eb9622efd
Gautam Suthar <iamgautamsuthar@outlook.com>
2026-01-02 18:56:05 +0530

    add: Components using zard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { IconsModule } from './shared/components/icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IconsModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
 })
 export class App {}
