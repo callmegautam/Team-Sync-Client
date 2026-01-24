678a7c4e33a2650e956123e64a4621b5cc5de959
Ishika Patel <ishikapatel1364@gmail.com>
2026-01-24 09:35:28 +0530

    refactor settings component to implement reactive forms and enhance validation

--- src/app/pages/client/dashboard/settings/settings.ts
+++ src/app/pages/client/dashboard/settings/settings.ts
@@ -2,11 +2,38 @@ import { ZardAvatarComponent } from '@/shared/components/avatar/avatar.component
 import { Component } from '@angular/core';
 import { ZardInputDirective } from '@/shared/components/input/input.directive';
 import { ZardButtonComponent } from '@/shared/components/button/button.component';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-settings',
-  imports: [ZardAvatarComponent, ZardInputDirective, ZardButtonComponent],
+  imports: [
+    ZardAvatarComponent,
+    ZardInputDirective,
+    ZardButtonComponent,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
-export class Settings {}
+export class Settings {
+  settingForm = new FormGroup({
+    // workspaceAvatar: new FormControl(''),
+    workspaceName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50),
+    ]),
+    workspaceDescription: new FormControl('', Validators.maxLength(255)),
+    workspaceInvite: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+    ]),
+  });
+
+  handleSetting() {
+    const { workspaceName, workspaceDescription, workspaceInvite } = this.settingForm.value;
+  }
+}
