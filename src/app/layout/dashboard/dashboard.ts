43de3a92992cbe15f3b67f85a0fe15152a6e73c3
Ishika Patel <ishikapatel1364@gmail.com>
2026-01-28 12:29:29 +0530

    add auth and basic layout

---
+++ src/app/layout/dashboard/dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home-layout',
+  imports: [RouterOutlet],
+  templateUrl: './dashboard.html',
+})
+export class DashboardLayout {}
