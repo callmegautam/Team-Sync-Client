85db7a7dbbba1a01fc3953eb58c63633cdcf2cda
aasthat648 <aasthat648@gmail.com>
2026-02-18 23:35:17 +0530

    again api is done

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  apicall: '/api/v1',
+  apicall: 'http://localhost:8080/api/v1',
+  // apicall: 'http://192.168.243.137:8080/api/v1',
+  // apicall: 'https://tsb.gautamsuthar.in/api/v1',
 };
